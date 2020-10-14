2aa4cc0b9d0b7a4cad0265e989f514fc73ef8d7f
Tariq96 <tareq.a.diab@gmail.com>
2020-10-14 22:43:20 +0200

    fixing frequency calculater fix 1

--- src/frequncy_calculator.py
+++ src/frequncy_calculator.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import time 
 import RPi.GPIO as gpio
 gpio.setmode(gpio.BOARD)
-
+gpio.setup(29,gpio.IN,pull_up_down=gpio.PUD_DOWN)
 ti=0
 intial=True
 T=0
@@ -10,11 +10,26 @@ def frequency_meter(channel):
     global ti , intial , T
     if intial:
         ti=time.time()
-        initial =False
-    else :
-        T=ti-time.time()
+        intial =False
+        print("rising1 at")
+        print(ti)
+        
+    if not intial :
+        tf=time.time()
+        T=tf-ti
         intial =True
-        print("the periodic timeis {}".format(T))
-gpio.add_event_detect(29, GPIO.RISING, callback=frequency_meter)  
+        print("rising2 at")
+        print(tf)
+        print("T={} , F={}".format(T,(1/T)))
+        
+gpio.add_event_detect(29, gpio.RISING)
+gpio.add_event_callback(29, callback=frequency_meter)  
+prevT=0
+while True:
+    if False:
+        print("T={} , F={}".format(T,(1/T)))
+    prevT=T
+    
+
 
     
