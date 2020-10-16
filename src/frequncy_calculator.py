d2d10f4689f941d64badf41e777f49aa754243e1
Tariq96 <tareq.a.diab@gmail.com>
2020-10-16 10:44:21 +0200

    adding rpm and rps calculator

--- src/frequncy_calculator.py
+++ src/frequncy_calculator.py
@@ -5,19 +5,38 @@ from read_PWM import reader
 import pigpio
 
 
+class motorspeed(self,pin,resolution):
+    def __init__(self,l,r):
+        self.pi=pigpio.pi()
+        self.motor=reader(pi,pin)
+    def RPS(self):
+        self.F=self.motor.frequency()
+        self.motor._high=None
+        self.motor._high_tick=None
+        self.motor._period=None
+
+        self.rps=self.F/resolution
+        return self.rps
+    def RPM(self):
+        self.rpm=self.RPS()*60
+        return self.rpm
+right_motor=motorspeed(5,20)
+while 1:
+    time.sleep(0.1)
+    print(right_motor.RPS())
+"""
 GPIO.setmode(GPIO.BOARD)
 pi = pigpio.pi()
 ml=reader(pi,5)
+
 while 1 :
     try:
         time.sleep(0.1)
-        print(ml.frequency(),ml.duty_cycle())
+        print(ml.frequency())
         ml._high=None
         ml._high_tick=None
         ml._period=None
     except:
         print("error occured")
         pass
-
-
-
+"""
--- src/install_pigpio.sh
+++
@@ -1,9 +0,0 @@
-mkdir -pv /etc/portage/patches/dev-libs/pigpio-70
-cd /etc/portage/patches/dev-libs/pigpio-70
-
-
-wget https://github.com/joan2937/pigpio/commit/55d8b880fca26ccf4b897bca1fe66796b9972345.patch -O 02-fix-net-comms-on-arm64.patch
-wget https://github.com/joan2937/pigpio/commit/5847d5c0991ba3d8b00fe11125aca5f74f72bc9d.patch -O 01-add-rpi4-arm64-support.patch
-
-cd
-emerge -v pigpio
