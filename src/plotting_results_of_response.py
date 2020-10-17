a49c01fa8651c900f2c395616ca149de6e35a68a
Tariq96 <tareq.a.diab@gmail.com>
2020-10-17 12:31:23 +0200

    modifing plotting app

--- src/plotting_results_of_response.py
+++ src/plotting_results_of_response.py
@@ -3,17 +3,33 @@ import matplotlib
 import numpy as np
 import pickle
     
-    
-infile=open("response_study_output/respone_analysis",'rb') 
-log =pickle.load(infile)   
-print(log[0])
+def step_f_plotting():
+    infile=open("response_study_output/respone_analysis",'rb') 
+    log =pickle.load(infile)   
+    print(log[0])
 
-fig , ax =plt.subplots()
-ax.plot(log[:,1],log[:,0])
-#ax.set(xlable='time ms',,title='step function response')
-ax.set_title('step function response')
-ax.set_xlabel('time (ms)')
-ax.set_ylabel('RPS')
-ax.grid()
-fig.savefig("response_study_output/step_response.png")
-plt.show()
+    fig , ax =plt.subplots()
+    ax.plot(log[:,1],log[:,0])
+    #ax.set(xlable='time ms',,title='step function response')
+    ax.set_title('step function response')
+    ax.set_xlabel('time (ms)')
+    ax.set_ylabel('RPS')
+    ax.grid()
+    fig.savefig("response_study_output/step_response.png")
+    plt.show()
+
+def DC_RPS_plotting():
+    infile=open("response_study_output/respone_analysis_DC_RPS",'rb') 
+    log =pickle.load(infile)   
+    print(log[0])
+
+    fig , ax =plt.subplots()
+    ax.plot(log[:,1],log[:,0])
+    #ax.set(xlable='time ms',,title='step function response')
+    ax.set_title('Duty Cycle - RPS relation Graph')
+    ax.set_xlabel('Duty Cycle')
+    ax.set_ylabel('RPS')
+    ax.grid()
+    fig.savefig("response_study_output/step_response.png")
+    plt.show()
+DC_RPS_plotting()
